fef651fd96fa4bc58fed8e928474f299694afa1f
0-vortex <0-vortex@users.noreply.github.com>
2021-10-17 22:41:23 +0000

    ci: add github release workflows 82399407cc07aa020363ab2d395a7e1a05a4119d

---
+++ _next/static/Nnfusyc1JY7ffwwz5digB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/645-9114661393070ced1216.js","static/chunks/pages/index-4b697dcad2875cf7efee.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
